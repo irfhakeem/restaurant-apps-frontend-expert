5b6b7eb8897eb34a3cf1ee8bdcb3b0c49cd585c1
IrfanHakimm <mirfanhakim08@gmail.com>
2024-12-25 13:42:03 +0700

    feat: indexedDB config

---
+++ src/scripts/data/api.js
@@ -0,0 +1,90 @@
+import config from "../globals/config";
+
+export async function ListOfRestaurants() {
+  try {
+    const response = await fetch(
+      `${config.baseUrl}${config.listOfRestaurants}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const responseJson = await response.json();
+    return responseJson.restaurants;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function DetailRestaurant(request) {
+  try {
+    const response = await fetch(
+      `${config.baseUrl}${config.detailRestaurant}${request.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const responseJson = await response.json();
+    return responseJson.restaurant;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function SearchRestaurant(request) {
+  try {
+    const response = await fetch(
+      `${config.baseUrl}${config.searchRestaurant}${request.query}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const responseJson = await response.json();
+    return responseJson.restaurants;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function AddReview(request) {
+  try {
+    const response = await fetch(`${config.baseUrl}/review`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: request.id,
+        name: request.name,
+        review: request.review,
+      }),
+    });
+    const responseJson = await response.json();
+    return responseJson.customerReviews;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function RestaurantImage(request) {
+  try {
+    const response = await fetch(
+      `${config.baseUrl}${config.restaurantImage}${request.pictureId}`,
+      {
+        method: "GET",
+      }
+    );
+    const blob = await response.blob();
+    return URL.createObjectURL(blob);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
---
+++ src/scripts/data/db.js
@@ -0,0 +1,43 @@
+import { openDB } from "idb";
+import config from "../globals/config";
+
+const DATABASE_NAME = config.DATABASE_NAME;
+const DATABASE_VERSION = config.DATABASE_VERSION;
+const OBJECT_STORE_NAME = config.OBJECT_STORE_NAME;
+
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade(database) {
+    if (!database.objectStoreNames.contains(OBJECT_STORE_NAME)) {
+      database.createObjectStore(OBJECT_STORE_NAME, { keyPath: "id" });
+    }
+  },
+});
+
+const Database = {
+  async getFavorite(id) {
+    if (!id) throw new Error("ID is required");
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+
+  async getAllFavorites() {
+    const favorites = await (await dbPromise).getAllKeys(OBJECT_STORE_NAME);
+    return favorites;
+  },
+
+  async addFavorite(id) {
+    if (!id) throw new Error("ID is required");
+    return (await dbPromise).put(OBJECT_STORE_NAME, { id });
+  },
+
+  async deleteFavorite(id) {
+    if (!id) throw new Error("ID is required");
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+  },
+
+  async isFavorite(id) {
+    const favorite = await this.getFavorite(id);
+    return !!favorite;
+  },
+};
+
+export default Database;
