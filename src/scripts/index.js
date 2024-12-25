e7ece3e0accc9c531c00968e2d272ee11385634c
IrfanHakimm <mirfanhakim08@gmail.com>
2024-12-25 13:45:04 +0700

    feat: register sw

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,39 +1,46 @@
-import "regenerator-runtime"; /* for async await transpile */
-import "../styles/main.css";
-import "../styles/detail.css";
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/detail.css';
 
-import App from "./views/app";
+import App from './views/app';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   const app = new App({
-    mainContent: document.querySelector("#mainContent"),
+    mainContent: document.querySelector('#mainContent'),
   });
 
   app.renderPage();
 
-  window.addEventListener("hashchange", () => {
+  window.addEventListener('hashchange', () => {
     app.renderPage();
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.querySelector(".navbar-toggle");
-  const menu = document.querySelector(".navbar-menu");
-  const body = document.querySelector("body");
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.querySelector('.navbar-toggle');
+  const menu = document.querySelector('.navbar-menu');
 
-  toggleButton.addEventListener("click", function () {
-    menu.classList.toggle("active");
+  toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('active');
   });
 
-  document.addEventListener("click", function (e) {
+  document.addEventListener('click', (e) => {
     if (!menu.contains(e.target) && !toggleButton.contains(e.target)) {
-      menu.classList.remove("active");
+      menu.classList.remove('active');
     }
   });
 });
 
-document.querySelector(".skip-link").addEventListener("click", function (e) {
+document.querySelector('.skip-link').addEventListener('click', (e) => {
   e.preventDefault();
-  const mainContent = document.getElementById("mainContent");
+  const mainContent = document.getElementById('mainContent');
   mainContent.focus();
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('sw.js').catch((error) => {
+      console.error(error);
+    });
+  });
+}
