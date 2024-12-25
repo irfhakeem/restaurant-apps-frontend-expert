bc81f065a57a1f5135bd625145e7dcf04ccbbb7c
IrfanHakimm <mirfanhakim08@gmail.com>
2024-12-25 13:42:33 +0700

    feat: favorite routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,8 +1,10 @@
-import HomePage from "../views/pages/home.js";
-import DetailPage from "../views/pages/detail.js";
+import HomePage from '../views/pages/home.js';
+import DetailPage from '../views/pages/detail.js';
+import FavoritePage from '../views/pages/favorite.js';
 
 export const page = {
-  "/": HomePage,
-  "/detail": (id) => DetailPage(id),
-  "/404": () => "<div><h1>Page Not Found</h1></div>",
+  '/': HomePage,
+  '/detail': (id) => DetailPage(id),
+  '/favorite': FavoritePage,
+  '/404': () => '<div><h1>Page Not Found</h1></div>',
 };
